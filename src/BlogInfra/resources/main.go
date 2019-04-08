59277c7ae5624983d90cfaef7eb444dc8dcb3ec2
Matt Simpson <m.simpson@outlook.com>
2019-04-09 11:22:21 +1200

    fix commit

---
+++ src/BlogInfra/resources/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Handler is executed by AWS Lambda in the main function. Once the request
+// is processed, it returns an Amazon API Gateway response object to AWS Lambda
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	//index, err := ioutil.ReadFile("public/index.html")
+
+	evt := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World",
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+	}
+
+	// if err != nil {
+	// 	return events.APIGatewayProxyResponse{}, err
+	// }
+
+	return evt, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
