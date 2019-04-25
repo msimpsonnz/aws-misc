08ca6a2e569d1c98d241421288fc9bebd8f19da9
Matt Simpson <m.simpson@outlook.com>
2019-04-25 22:14:34 +1200

    comment out SQS to get basics working

--- cdk/lib/shared-stack.ts
+++ cdk/lib/shared-stack.ts
@@ -1,22 +1,22 @@
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/cdk');
-//import dynamo = require('@aws-cdk/aws-dynamodb');
+// import sqs = require('@aws-cdk/aws-sqs');
+// import cdk = require('@aws-cdk/cdk');
+// //import dynamo = require('@aws-cdk/aws-dynamodb');
 
-export class SharedStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+// export class SharedStack extends cdk.Stack {
+//     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+//         super(scope, id, props);
 
-        //TODO: Add Dynamo config
+//         //TODO: Add Dynamo config
 
-        // const lambda_api = new lambda.Function(this, 'Lambda', {
-        //   runtime: lambda.Runtime.Go1x,
-        //   code: lambda.Code.asset('resources'),
-        //   handler: 'main'               
-        // });
+//         // const lambda_api = new lambda.Function(this, 'Lambda', {
+//         //   runtime: lambda.Runtime.Go1x,
+//         //   code: lambda.Code.asset('resources'),
+//         //   handler: 'main'               
+//         // });
 
-        //Queue for Lambda Starter
-        const queue = new sqs.Queue(this, 'CdkQueue', {
-            visibilityTimeoutSec: 300
-        });
-    }
-}
+//         //Queue for Lambda Starter
+//         const queue = new sqs.Queue(this, 'CdkQueue', {
+//             visibilityTimeoutSec: 300
+//         });
+//     }
+// }
