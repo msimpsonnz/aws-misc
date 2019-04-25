2bf888b3e0ffa3932476216908e18378fc940cb2
Matt Simpson <m.simpson@outlook.com>
2019-04-25 21:57:48 +1200

    update to v0.29 change to single file deployment to simplify and test

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,41 +1,155 @@
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/cdk');
-//import dynamo = require('@aws-cdk/aws-dynamodb');
+import cdk = require('@aws-cdk/cdk')
+import codebuild = require('@aws-cdk/aws-codebuild');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
 import lambda = require('@aws-cdk/aws-lambda');
-import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
+import secretsmanager = require('@aws-cdk/aws-secretsmanager');
+import ssm = require('@aws-cdk/aws-ssm');
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    //TODO: Add Dynamo config
+        const secretArnParam = new ssm.ParameterStoreString(this, 'secretMgrArn', {
+            parameterName: 'secretMgrArn'
+        }).stringValue;;
 
-    // const lambda_api = new lambda.Function(this, 'Lambda', {
-    //   runtime: lambda.Runtime.Go1x,
-    //   code: lambda.Code.asset('resources'),
-    //   handler: 'main'               
-    // });
+        const secret = secretsmanager.Secret.import(this, 'GitHubAccessToken', {
+            secretArn: secretArnParam
+        });
 
-    //Queue for Lambda Starter
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeoutSec: 300
-    });
+        const pipeline = new codepipeline.Pipeline(this, 'mjsdemo-cdk-pipeline', {
+            pipelineName: 'mjsdemo-cdk-pipeline',
+        });
 
-    //Lambda to kick off batch to SQS  
-    const lambda_starter = new lambda.Function(this, 'Lambda', {
-      runtime: lambda.Runtime.DotNetCore21,
-      code: lambda.Code.asset('../Functions/deploy/lamb-net.zip'),
-      handler: 'lamb-net::StarterFunc.Functions::Get'               
-    });
+        const cdkSourceOutput = new codepipeline.Artifact();
+        const cdkSourceAction = new codepipeline_actions.GitHubSourceAction({
+            actionName: 'CDK_Source',
+            owner: 'msimpsonnz',
+            repo: 'sls-net',
+            oauthToken: secret.secretJsonValue('GitHubPAT'),
+            output: cdkSourceOutput,
+            branch: 'master'
+        });
+
+        const lambdaSourceOutput = new codepipeline.Artifact();
+        const lambdaSourceAction = new codepipeline_actions.GitHubSourceAction({
+            actionName: 'Lambda_Source',
+            owner: 'msimpsonnz',
+            repo: 'cdk-ci-cd',
+            oauthToken: secret.secretJsonValue('GitHubPAT'),
+            output: lambdaSourceOutput,
+            branch: 'master'
+        });
+
+        pipeline.addStage({
+            name: 'Source',
+            actions: [cdkSourceAction, lambdaSourceAction],
+        });
+
+        const cdkBuildProject = new codebuild.Project(this, 'CdkBuildProject', {
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_NODEJS_10_1_0,
+            },
+            buildSpec: {
+                version: '0.2',
+                phases: {
+                    install: {
+                        commands: [
+                            'cd ./cdk',
+                            'npm install'
+                        ]
+                    },
+                    build: {
+                        commands: [
+                            'cd ./src',
+                            'npm run build',
+                            'npm run cdk synth LambdaStack -- -o .',
+                        ],
+                    },
+                },
+                artifacts: {
+                    files: 'LambdaStack.template.yaml',
+                },
+            },
+        });
+
+        const cdkBuildOutput = new codepipeline.Artifact();
+        const cdkBuildAction = new codepipeline_actions.CodeBuildAction({
+            actionName: 'CDK_Build',
+            project: cdkBuildProject,
+            input: cdkSourceOutput,
+            output: cdkBuildOutput,
+        });
+
+        const lambdaBuildProject = new codebuild.Project(this, 'LambdaBuildProject', {
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.UBUNTU_14_04_GOLANG_1_10,
+            },
+            buildSpec: {
+                version: '0.2',
+                phases: {
+                    install: {
+                        commands: [
+                            'ln -s "${CODEBUILD_SRC_DIR}/src/resources" "/go/src/handler"',
+                            'go get golang.org/x/lint/golint',
+                            'go get -u github.com/stretchr/testify'
+                        ]
+                    },
+                    pre_build: {
+                        commands: [
+                            'cd "/go/src/handler"',
+                            'go get ./...',
+                            'golint -set_exit_status',
+                            'go tool vet .',
+                            'go test .'
+                        ]
+                    },
+                    build: {
+                        commands: 'go build -o main',
+                    },
+                },
+                artifacts: {
+                    type: 'zip'
+                },
+            },
+        });
 
 
-    const lambda_worker = new lambda.Function(this, 'Lambda', {
-      runtime: lambda.Runtime.DotNetCore21,
-      code: lambda.Code.asset('../Functions/deploy/WorkerFunc.zip'),
-      handler: 'WorkerFunc::WorkerFunc.Functions::Get'               
-    });
+        const lambdaBuildOutput = new codepipeline.Artifact();
+        const lambdaBuildAction = new codepipeline_actions.CodeBuildAction({
+          actionName: 'Lambda_Build',
+          project: lambdaBuildProject,
+          input: lambdaSourceOutput,
+          output: lambdaBuildOutput,
+        });
 
-    lambda_worker.addEventSource(new SqsEventSource(queue));
+        pipeline.addStage({
+            name: 'Build',
+            actions: [cdkBuildAction, lambdaBuildAction],
+        });
 
-  }
+        const lambdaCode = lambda.Code.cfnParameters();
+        // finally, deploy your Lambda Stack
+        pipeline.addStage({
+            name: 'Deploy',
+            actions: [
+                new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+                    actionName: 'Lambda_CFN_Deploy',
+                    templatePath: cdkBuildOutput.atPath('LambdaStack.template.yaml'),
+                    stackName: 'LambdaStackDeployedName',
+                    adminPermissions: true,
+                    parameterOverrides: {
+                        ...lambdaCode.assign(lambdaBuildOutput.s3Coordinates),
+                    },
+                    extraInputs: [
+                        lambdaBuildOutput,
+                    ],
+                }),
+            ],
+        });
+
+
+
+    }
 }
---
+++ cdk/lib/lambda-stack.ts
@@ -0,0 +1,35 @@
+import cdk = require('@aws-cdk/cdk');
+//import dynamo = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources';
+
+
+export class LambdaStarterStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaCode = lambda.Code.cfnParameters();
+    //Lambda to kick off batch to SQS  
+    new lambda.Function(this, 'LambdaStarter', {
+      runtime: lambda.Runtime.DotNetCore21,
+      code: lambdaCode,
+      handler: 'lamb-net::StarterFunc.Functions::Get'               
+    });
+  }
+}
+
+export class LambdaWorkerStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaCode = lambda.Code.cfnParameters();
+    new lambda.Function(this, 'LambdaWorker', {
+      runtime: lambda.Runtime.DotNetCore21,
+      code: lambdaCode,
+      handler: 'WorkerFunc::WorkerFunc.Functions::Get'               
+    });
+
+    //lambda_worker.addEventSource(new SqsEventSource(props.));
+  
+  }
+}
