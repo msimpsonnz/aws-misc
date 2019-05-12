3bedd1dbf1c6c01cad8c7c12cf005251c4acf24a
Matt Simpson <m.simpson@outlook.com>
2019-05-13 09:25:53 +1200

    add amplify project

--- sns-subs/src/sls/lambda-sns-sub/publish/build.sh
+++ sns-subs/src/sls/lambda-sns-sub/publish/build.sh
@@ -2,4 +2,4 @@
 rm bin/main
 rm bin/main.zip
 GOOS=linux GOARCH=amd64 go build -o bin/main main.go
-zip bin/main.zip bin/main
+zip -j bin/main.zip bin/main
---
+++ sns-subs/src/web/aws-amplify-vue-master/src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="form">
+    <div class="inputRow">
+        <label class="inputLabel">Message</label>
+        <input class="input"/>
+    </div>
+    <div class="actionRow">
+      <button class="action" v-on:click="save">Send</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Amplify, { API } from 'aws-amplify';
+
+
+let apiName = 'apie495ddea'; // replace this with your api name.
+let path = '/msg'; //replace this with the path you have configured on your API
+let myInit = {
+    body: {'body': 'test from amplify'}, // replace this with attributes you need
+    headers: {} // OPTIONAL
+}
+
+
+
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    save() {
+      API.post(apiName, path, myInit).then(response => {
+          // Add your code here
+      }).catch(error => {
+          console.log(error.response)
+      });
+    }
+  }
+}
+</script>
