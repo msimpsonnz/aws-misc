6e33731876d7da6dcf7390fa03825599695c2aa0
Matt Simpson <m.simpson@outlook.com>
2019-06-26 21:41:26 +1200

    fix express

--- ecs-iam/app/server.js
+++ ecs-iam/app/server.js
@@ -4,14 +4,16 @@ const express = require('express');
 const request = require('request');
 
 // Constants
-const PORT = 8080;
+const PORT = 80;
 const HOST = '0.0.0.0';
 
 // App
+const credUrl = process.env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
+
 const app = express();
 app.get('/', (req, res) => {
-    request.get(`http://169.254.170.2${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}`);
-    res.send();
+    request.get(`http://169.254.170.2${credUrl}`);
+    res.send(request);
 });
 
 app.listen(PORT, HOST);
