d20d2c08c3ea003fe784f5b5d714087f8f86eef8
Matt Simpson <m.simpson@outlook.com>
2019-07-01 12:44:43 +1200

    sample chat app

---
+++ amp-bot/front/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Amplify, { Interactions } from 'aws-amplify';
+import { ChatBot, AmplifyTheme } from 'aws-amplify-react';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-east-1:1xxxx',
+    region: 'us-east-1'
+  },
+  Interactions: {
+    bots: {
+      "BookTrip": {
+        "name": "BookTrip",
+        "alias": "$LATEST",
+        "region": "us-east-1"
+      },
+    }
+  }
+});
+
+// Imported default theme can be customized by overloading attributes
+const myTheme = {
+  ...AmplifyTheme,
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: '#ff6600'
+  }
+};
+
+class App extends Component {
+
+  handleComplete(err, confirmation) {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
+    }
+
+    alert('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Trip booked. Thank you! what would you like to do next?';
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to ChatBot Demo</h1>
+        </header>
+        <ChatBot
+          title="My Bot"
+          theme={myTheme}
+          botName="BookTrip"
+          welcomeMessage="Welcome, how can I help you today?"
+          onComplete={this.handleComplete.bind(this)}
+          clearOnComplete={true}
+          conversationModeOn={false}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
