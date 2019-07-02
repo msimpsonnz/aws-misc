4940a834af0dbefd1fff2b0b82a1373386470641
Matt Simpson <m.simpson@outlook.com>
2019-07-03 09:48:24 +1200

    working version of custom runtime

---
+++ lambda-runtime/CustomRuntimeFunction/Amazon.Lambda.Serialization.Json/JsonSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Amazon.Lambda.Core;
+
+namespace Amazon.Lambda.Serialization.Json
+{
+    public class JsonSerializer : ILambdaSerializer
+    {
+        public JsonSerializer()
+        {
+        }
+
+        public T Deserialize<T>(Stream requestStream)
+        {
+            var json = new StreamReader(requestStream).ReadToEnd();
+            return System.Text.Json.Serialization.JsonSerializer.Parse<T>(json);
+        }
+
+        public void Serialize<T>(T response, Stream responseStream)
+        {
+            System.Text.Json.Serialization.JsonSerializer.WriteAsync<T>(response, responseStream);
+
+        }
+    }
+}
