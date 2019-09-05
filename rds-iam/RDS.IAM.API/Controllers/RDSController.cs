31cf9aa8073d0a12cf4af5fa924f0f3679ab9268
Matt Simpson <m.simpson@outlook.com>
2019-09-05 15:23:46 +1200

    fix build

--- rds-iam/RDS.IAM.API/Controllers/RDSController.cs
+++ rds-iam/RDS.IAM.API/Controllers/RDSController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
-using MySqlConnector;
 
 namespace RDS.IAM.API.Controllers
 {
