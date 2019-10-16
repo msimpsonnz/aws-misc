e3969cc3370cc0d19096123ce073aba593d4fd0a
Matt Simpson <m.simpson@outlook.com>
2019-10-17 09:26:56 +1300

    add lex interop with connect

--- connect-lex/functions/handler.py
+++ connect-lex/functions/handler.py
@@ -1,18 +1,208 @@
+"""
+This sample demonstrates an implementation of the Lex Code Hook Interface
+in order to serve a sample bot which manages reservations for hotel rooms and car rentals.
+Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console
+as part of the 'BookTrip' template.
+
+For instructions on how to set up and test this bot, as well as additional samples,
+visit the Lex Getting Started documentation http://docs.aws.amazon.com/lex/latest/dg/getting-started.html.
+"""
+
 import json
+import datetime
+import time
 import os
+import dateutil.parser
+import logging
 import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 client = boto3.client('connect', region_name='ap-southeast-2')
 
-def lambda_handler(event, context):
-    print(event['DestinationPhoneNumber'])
-    client.start_outbound_voice_contact(
-        DestinationPhoneNumber=event['DestinationPhoneNumber'],
+# --- Helpers that build all of the responses ---
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def confirm_intent(session_attributes, intent_name, slots, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ConfirmIntent',
+            'intentName': intent_name,
+            'slots': slots,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+# --- Helper Functions ---
+
+
+def safe_int(n):
+    """
+    Safely convert n value to int.
+    """
+    if n is not None:
+        return int(n)
+    return n
+
+
+def try_ex(func):
+    """
+    Call passed in function in try block. If KeyError is encountered return None.
+    This function is intended to be used to safely access dictionary.
+
+    Note that this function would have negative impact on performance.
+    """
+
+    try:
+        return func()
+    except KeyError:
+        return None
+
+
+def generate_car_price(location, days, age, car_type):
+    """
+    Generates a number within a reasonable range that might be expected for a flight.
+    The price is fixed for a given pair of locations.
+    """
+
+    car_types = ['economy', 'standard', 'midsize', 'full size', 'minivan', 'luxury']
+    base_location_cost = 0
+    for i in range(len(location)):
+        base_location_cost += ord(location.lower()[i]) - 97
+
+    age_multiplier = 1.10 if age < 25 else 1
+    # Select economy is car_type is not found
+    if car_type not in car_types:
+        car_type = car_types[0]
+
+    return days * ((100 + base_location_cost) + ((car_types.index(car_type.lower()) * 50) * age_multiplier))
+
+
+def isvalid_date(date):
+    try:
+        dateutil.parser.parse(date)
+        return True
+    except ValueError:
+        return False
+
+
+def get_day_difference(later_date, earlier_date):
+    later_datetime = dateutil.parser.parse(later_date).date()
+    earlier_datetime = dateutil.parser.parse(earlier_date).date()
+    return abs(later_datetime - earlier_datetime).days
+
+
+def add_days(date, number_of_days):
+    new_date = dateutil.parser.parse(date).date()
+    new_date += datetime.timedelta(days=number_of_days)
+    return new_date.strftime('%Y-%m-%d')
+
+
+def build_validation_result(isvalid, violated_slot, message_content):
+    return {
+        'isValid': isvalid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+
+
+def connect_callback(intent_request):
+
+    phoneNumber = try_ex(lambda: intent_request['currentIntent']['slots']['PhoneNumber'])
+
+    session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
+
+    print(phoneNumber)
+    response = client.start_outbound_voice_contact(
+        DestinationPhoneNumber=phoneNumber,
         ContactFlowId=os.environ['ContactFlowId'],
         InstanceId=os.environ['InstanceId'],
         QueueId=os.environ['QueueId']
+        )
+    print(response)
+
+    return close(
+        session_attributes,
+        'Fulfilled',
+        {
+            'contentType': 'PlainText',
+            'content': 'Ok, we are calling you back now'
+        }
     )
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Ok')
-    }
+
+
+
+
+# --- Intents ---
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'CallBack':
+        return connect_callback(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+# --- Main handler ---
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    #os.environ['TZ'] = 'America/New_York'
+    #time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
