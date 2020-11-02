b3a91238157b4a4c1a6c64648c5f3c8b62adc337
Matt Simpson <m.simpson@outlook.com>
2020-11-03 12:39:24 +1300

    add std sqs for comparison

--- sqs-fifo-single/lib/sqs-fifo-single-stack.ts
+++ sqs-fifo-single/lib/sqs-fifo-single-stack.ts
@@ -8,11 +8,11 @@ export class SqsFifoSingleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'queue', {
+    const queueFifo = new sqs.Queue(this, 'queueFifo', {
       fifo: true
     });
 
-    const fn = new lambda.Function(this, 'fnEventLogger', {
+    const fnFifo = new lambda.Function(this, 'fnFifo', {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       timeout: cdk.Duration.seconds(20),
@@ -25,14 +25,43 @@ export class SqsFifoSingleStack extends cdk.Stack {
       reservedConcurrentExecutions: 1
     });
 
-    fn.addEventSource(
-      new SqsEventSource(queue, {
+    fnFifo.addEventSource(
+      new SqsEventSource(queueFifo, {
         batchSize: 1,
       })
     );
 
-    new cdk.CfnOutput(this, 'queueUrl', {
-      value: queue.queueUrl
+    new cdk.CfnOutput(this, 'queueFifoUrl', {
+      value: queueFifo.queueUrl
+    })
+
+
+    const queueStd = new sqs.Queue(this, 'queueStd')
+    const queueStdDL = new sqs.Queue(this, 'queueStdDL')
+
+    const fnStd = new lambda.Function(this, 'fnStd', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      timeout: cdk.Duration.seconds(20),
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) { 
+          console.log(JSON.stringify(event));
+          await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+      `),
+      reservedConcurrentExecutions: 1,
+      deadLetterQueue: queueStdDL,
+      deadLetterQueueEnabled: true
+    });
+
+    fnStd.addEventSource(
+      new SqsEventSource(queueStd, {
+        batchSize: 1,
+      })
+    );
+
+    new cdk.CfnOutput(this, 'queueStdUrl', {
+      value: queueStd.queueUrl
     })
 
   }
